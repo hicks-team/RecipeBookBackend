69030fd4f62db1d7c04b19c927493e4a7c858637
Eric Hicks <ehicks05@yahoo.com>
2022-06-19 16:26:58 -0500

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { serve, setup } from 'swagger-ui-express';
-import {spec} from './swagger';
 import router from './api';
 
 const port = process.env.PORT || 8989;
@@ -11,8 +9,9 @@ const app = express();
 app.use(cors());
 
 app.use(morgan('dev'));
-app.use('/docs', serve, setup(spec));
 app.use('/api', router);
+
+app.get('/hello', async (req, res) => res.json({message: 'hello'}) )
 
 app.listen(port, () => {
   console.log('server start');
